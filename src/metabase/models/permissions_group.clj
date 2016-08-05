0ab87bb27f7aff79b0ac9fed4c13e343735de3fe
Cam Saül <cammsaul@gmail.com>
2016-08-05 12:01:33 -0700

    Test fix :wrench:

--- src/metabase/models/permissions_group.clj
+++ src/metabase/models/permissions_group.clj
@@ -5,14 +5,40 @@
 
 (i/defentity PermissionsGroup :permissions_group)
 
+
+;;; magic permissions groups getter helper fns
+
+(def ^{:arglists '([])}
+  ^metabase.models.permissions_group.PermissionsGroupInstance
+  default
+  "Fetch the `Default` permissions group, creating it if needed."
+  (memoize (fn []
+             (or (db/select-one PermissionsGroup
+                   :name "Default")
+                 (db/insert! PermissionsGroup
+                   :name "Default")))))
+
+(def
+  ^{:arglists '([])}
+  ^metabase.models.permissions_group.PermissionsGroupInstance
+  admin
+  "Fetch the `Admin` permissions group, creating it if needed."
+  (memoize (fn []
+             (or (db/select-one PermissionsGroup
+                   :name "Admin")
+                 (db/insert! PermissionsGroup
+                   :name "Admin")))))
+
+
 (defn- throw-exception-when-editing-magic-group
   "Make sure we're not trying to edit/delete one of the magic groups, or throw an exception."
-  [{group-name :name}]
-  {:pre [(string? group-name)]}
-  (when (= group-name "Default")
+  [{id :id}]
+  {:pre [(integer? id)]}
+  (when (= id (:id (default)))
     (throw (Exception. "You cannot delete the 'Default' permissions group!")))
-  (when (= group-name "Admin")
+  (when (= id (:id (admin)))
     (throw (Exception. "You cannot delete the 'Admin' permissions group!"))))
+
 
 (defn- pre-cascade-delete [{id :id, :as group}]
   (throw-exception-when-editing-magic-group group)
@@ -21,7 +47,7 @@
   (db/cascade-delete! 'SchemaPermissions          :group_id id)
   (db/cascade-delete! 'PermissionsGroupMembership :group_id id))
 
-(defn- pre-update [{id :id, :as group}]
+(defn- pre-update [group]
   (throw-exception-when-editing-magic-group group)
   group)
 
@@ -29,22 +55,3 @@
   i/IEntity (merge i/IEntityDefaults
                    {:pre-cascade-delete pre-cascade-delete
                     :pre-update         pre-update}))
-
-
-;;; magic permissions groups getter helper fns
-
-(defn default
-  "Fetch the `Default` permissions group, creating it if needed."
-  ^metabase.models.permissions_group.PermissionsGroupInstance []
-  (or (db/select-one PermissionsGroup
-        :name "Default")
-      (db/insert! PermissionsGroup
-        :name "Default")))
-
-(defn admin
-  "Fetch the `Admin` permissions group, creating it if needed."
-  ^metabase.models.permissions_group.PermissionsGroupInstance []
-  (or (db/select-one PermissionsGroup
-        :name "Admin")
-      (db/insert! PermissionsGroup
-        :name "Admin")))
