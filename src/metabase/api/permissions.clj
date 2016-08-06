3015763f1d9fe5f2b1ccf5e5d508a99162ebd1f8
Cam Saül <cammsaul@gmail.com>
2016-08-05 19:00:11 -0700

    GET /api/permissions/group/:id endpoint

--- src/metabase/api/permissions.clj
+++ src/metabase/api/permissions.clj
@@ -3,7 +3,8 @@
   (:require [compojure.core :refer [GET]]
             [metabase.api.common :refer :all]
             [metabase.db :as db]
-            #_(metabase.models [permissions-group :refer [PermissionsGroup]])))
+            (metabase.models [permissions-group :refer [PermissionsGroup]]
+                             [permissions-group-membership :refer [PermissionsGroupMembership]])))
 
 (defendpoint GET "/groups"
   "Fetch all `PermissionsGroups`."
@@ -15,6 +16,23 @@
                          [:= :pg.id :pgm.group_id]]
              :group-by  [:pg.id :pg.name]
              :order-by  [:%lower.pg.name]}))
+
+(defendpoint GET "/group/:id"
+  "Fetch details for a specific `PermissionsGroup`."
+  [id ]
+  (check-superuser)
+  (let [group (PermissionsGroup id)]
+    (assoc group
+      :members   (db/query {:select   [:u.first_name :u.last_name :u.email [:u.id :user_id] [:pgm.id :membership_id]]
+                            :from     [[:permissions_group_membership :pgm]]
+                            :join     [[:core_user :u] [:= :pgm.user_id :u.id]]
+                            :where    [:= :pgm.group_id id]
+                            :order-by [:pgm.id]})
+      :databases (db/query {:select   [:d.name [:d.id :database_id] [:dp.id :permissions_id] :unrestricted_schema_access :native_query_write_access]
+                            :from     [[:database_permissions :dp]]
+                            :join     [[:metabase_database :d] [:= :dp.database_id :d.id]]
+                            :where    [:= :dp.group_id 1]
+                            :order-by [:%lower.d.name]}))))
 
 
 (define-routes)
